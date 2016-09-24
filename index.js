0de440594651ac6bae2d0f8c8468d94ae854648a
Luke Edwards <luke.edwards05@gmail.com>
2016-09-24 15:16:05 -0700

    use tabs

--- index.js
+++ index.js
@@ -2,37 +2,37 @@ var exec = require('execa').shell;
 var NAME = 'fly-shell';
 
 module.exports = function () {
-  this.shell = function () {
-    var self = this;
-    var args = [].slice.call(arguments);
-    var opts = args.pop() || {};
-    var cmd = args.shift();
-    var isGlob = opts.glob || 0;
+	this.shell = function () {
+		var self = this;
+		var args = [].slice.call(arguments);
+		var opts = args.pop() || {};
+		var cmd = args.shift();
+		var isGlob = opts.glob || 0;
 
-    var runWith = function (str) {
-      // use file or glob
-      var c = cmd.replace(/\$file/gi, str);
-      // pass all args to execa
-      return exec.apply(self, [c, args, opts]).then(function (res) {
-        var str = NAME + ':' + (isGlob ? '\n\t' : ' ');
-        str += res.stdout.replace(/\n/g, isGlob ? '\n\t' : '\n');
-        self.log(str);
-      }).catch(function (err) {
-        self.emit('plugin_error', {
-          plugin: NAME,
-          error: err.message
-        });
-      });
-    };
+		var runWith = function (str) {
+			// use file or glob
+			var c = cmd.replace(/\$file/gi, str);
+			// pass all args to execa
+			return exec.apply(self, [c, args, opts]).then(function (res) {
+				var str = NAME + ':' + (isGlob ? '\n\t' : ' ');
+				str += res.stdout.replace(/\n/g, isGlob ? '\n\t' : '\n');
+				self.log(str);
+			}).catch(function (err) {
+				self.emit('plugin_error', {
+					plugin: NAME,
+					error: err.message
+				});
+			});
+		};
 
-    if (isGlob) {
-      runWith(this._.globs[0]);
-    } else {
-      this.unwrap(function (files) {
-        files.forEach(runWith);
-      });
-    }
+		if (isGlob) {
+			runWith(this._.globs[0]);
+		} else {
+			this.unwrap(function (files) {
+				files.forEach(runWith);
+			});
+		}
 
-    return this;
-  };
+		return this;
+	};
 };
