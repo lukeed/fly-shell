277831905a52215676e8a669577e64c6370bf531
Luke Edwards <luke.edwards05@gmail.com>
2016-10-26 11:25:35 +0200

    upgrade plugin logic

--- index.js
+++ index.js
@@ -1,37 +1,47 @@
-var execa = require('execa').shell;
+'use strict';
 
-var NAME = 'fly-shell';
+const format = require('path').format;
+const execa = require('execa').shell;
+const NAME = 'fly-shell';
 
 module.exports = function () {
-	this.shell = function () {
-		var self = this;
-		var args = [].slice.call(arguments);
-		var opts = args.pop() || {};
-		var cmd = args.shift();
-		var isGlob = opts.glob || 0;
+	const setError = msg => this.emit('plugin_error', {
+		error: msg,
+		plugin: NAME
+	});
 
-		var runWith = function (str) {
+	this.plugin('shell', {every: 0}, function * (files, cmd, opts) {
+		opts = opts || {};
+
+		if (typeof cmd !== 'string') {
+			opts = cmd;
+			cmd = opts.cmd;
+		}
+
+		const args = opts.args || [];
+
+		if (!cmd) {
+			return setError('No command received!');
+		}
+
+		const isGlob = opts.glob || 0;
+
+		// output header
+		const head = `${NAME}:${isGlob ? '\n\t' : ' '}`;
+		const tail = isGlob ? '\n\t' : '\n';
+
+		const runWith = str => {
 			// use file or glob
-			var c = cmd.replace(/\$file/gi, str);
+			const c = cmd.replace(/\$(file|glob)/gi, str);
 			// pass all args to execa
-			return execa.apply(self, [c, args, opts]).then(function (res) {
-				self.log(NAME + ':' + (isGlob ? '\n\t' : ' ') + res.stdout.replace(/\n/g, isGlob ? '\n\t' : '\n'));
-			}).catch(function (err) {
-				self.emit('plugin_error', {
-					plugin: NAME,
-					error: err.message
-				});
+			return execa.apply(this, [c, args, opts]).then(res => {
+				this.$.log(head + res.stdout.replace(/\n/g, tail));
+			}).catch(err => {
+				setError(err.message);
 			});
 		};
 
-		if (isGlob) {
-			runWith(this._.globs[0]);
-		} else {
-			this.unwrap(function (files) {
-				files.forEach(runWith);
-			});
-		}
-
-		return this;
-	};
+		const src = isGlob ? this._.globs : files.map(format);
+		return yield Promise.all(src.map(runWith));
+	});
 };
